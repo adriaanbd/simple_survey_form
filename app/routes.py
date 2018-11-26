45e42e4c4126f8ce9debd29edfb0ac8fe4b4f4af
Adriaan Beiertz <22228397+adriaanbd@users.noreply.github.com>
2018-11-26 17:58:10 -0500

    Form works and submits name  with flash success message

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,12 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.form import SurveyForm
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     form = SurveyForm()
     if form.validate_on_submit():
-        flash(f"Thanks {form.name}, your form has been submitted!")
-        return redirect('/index')
+        flash(f"Thanks {form.name.data}, your form has been submitted!")
+        return redirect(url_for('index'))
     return render_template('survey.html', form=form)
