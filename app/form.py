fd2eaf6bc5d2f063d3e08800395a92765fc07b34
Adriaan Beiertz <22228397+adriaanbd@users.noreply.github.com>
2018-11-26 15:35:12 -0500

    Added submit field

--- app/form.py
+++ app/form.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, SubmitField
+from wtforms.validators import InputRequired
 
 class SurveyForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    
+    name = StringField('Name', validators=[InputRequired()])
+    submit = SubmitField('Submit')
+
     
