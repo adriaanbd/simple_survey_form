0cbfc8f0cf3f6392e3c10963e5e3e41eedc6facb
Adriaan Beiertz <22228397+adriaanbd@users.noreply.github.com>
2018-11-26 14:34:00 -0500

    Modified index connecting SurveyForm to html template

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
-from app import app
 from flask import render_template
+from app import app
+from app.form import SurveyForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('survey.html')
+    form = SurveyForm()
+    return render_template('survey.html', form=form)
