4814375e7eeb7a20deb7f6adda2ac346d3484bcd
Adriaan Beiertz <22228397+adriaanbd@users.noreply.github.com>
2018-11-26 15:36:59 -0500

    Added validate on submit, flash message and redirect

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,12 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.form import SurveyForm
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     form = SurveyForm()
+    if form.validate_on_submit():
+        flash(f"Thanks {form.name}, your form has been submitted!")
+        return redirect('/index')
     return render_template('survey.html', form=form)
