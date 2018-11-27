26cf39d40af4f38c0f49676a88006f01d2b6e5d1
Adriaan Beiertz <22228397+adriaanbd@users.noreply.github.com>
2018-11-27 17:18:30 -0500

    Added email, gender, age and path choices in WTForms and jinja tags in html. Locators need to be modified.

--- app/form.py
+++ app/form.py
@@ -1,9 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import (StringField, SubmitField, IntegerField, RadioField, SelectField)
 from wtforms.validators import DataRequired
 
 class SurveyForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    age = IntegerField('Age')
+    gender = RadioField('Gender', choices=[
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('neutral', 'Neutral'),
+        ('undisclosed', 'No preference')
+    ])
+    path = SelectField(
+        'Which web development path do you identify yourself with?',
+        choices=[
+            ('front', 'Front End'),
+            ('back', 'Back End'),
+            ('full','Full Stack')
+        ])
     submit = SubmitField('Submit')
 
     
