78cb6461efdce17325167c0c8c7217c27e8e9cb9
Adriaan Beiertz <22228397+adriaanbd@users.noreply.github.com>
2018-11-28 12:57:44 -0500

    Added checkbox in form.py and their jinja tags in survey.html

--- app/form.py
+++ app/form.py
@@ -1,5 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import (StringField, SubmitField, IntegerField, RadioField, SelectField)
+from wtforms import (
+    StringField, SubmitField, IntegerField, RadioField, SelectField, SelectMultipleField
+    )
+from wtforms.widgets import CheckboxInput, ListWidget
 from wtforms.validators import DataRequired
 
 class SurveyForm(FlaskForm):
@@ -10,15 +13,25 @@ class SurveyForm(FlaskForm):
         ('male', 'Male'),
         ('female', 'Female'),
         ('neutral', 'Neutral'),
-        ('undisclosed', 'No preference')
+        ('undisclosed', 'No preference'),
     ])
     path = SelectField(
         'Which web development path do you identify yourself with?',
         choices=[
             ('front', 'Front End'),
             ('back', 'Back End'),
-            ('full','Full Stack')
+            ('full','Full Stack'),
         ])
+    language = SelectMultipleField(
+        'What programming languages do you use?',
+        choices = [
+        ('js', 'JavaScript'),
+        ('ts', 'TypeScript'),
+        ('py', 'Python'),
+        ('csharp', 'C#'),
+        ('java', 'Java'),
+        ('other', 'Other'),
+        ],
+        option_widget=CheckboxInput(),
+        widget=ListWidget(prefix_label=False))
     submit = SubmitField('Submit')
-
-    
