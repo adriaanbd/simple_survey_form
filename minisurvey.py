74d6e9b051c1205cafdfe23bd86474ec2b594ef0
Adriaan Beiertz <22228397+adriaanbd@users.noreply.github.com>
2018-12-01 15:16:03 -0500

    Added flask shell context.

--- minisurvey.py
+++ minisurvey.py
@@ -1,1 +1,10 @@
-from app import app
+from app import app, db
+from app.models import Voter, Answer
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        'db': db,
+        'Voter': Voter,
+        'Answer': Answer,
+    }
